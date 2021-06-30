8ad61cf6fad35ac0c84ae40d349bd00853bbeef9
Hannes Karppila <hannes.karppila@gmail.com>
2021-06-30 16:06:29 +0000

    Clean up unused files

--- src/ls.rs
+++
@@ -1,6 +0,0 @@
-            let mut entries = std::fs::read_dir(".")
-                .unwrap()
-                .map(|res| res.map(|e| e.path()))
-                .collect::<Result<Vec<_>, std::io::Error>>()
-                .unwrap();
-            println!("{:?}", entries);
